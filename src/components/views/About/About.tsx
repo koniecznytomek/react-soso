34a32e4a9d7243e9a3f766e4ae5fa9365a2a71a4
koniecznytomek <tomek@tomekkonieczny.com>
2021-01-13 21:48:48 +0100

    Add about page

--- src/components/views/About/About.tsx
+++ src/components/views/About/About.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from './About.module.scss';
+import { useThemeObserver } from '../../../hooks/useThemeObserver';
+import { motion } from 'framer-motion';
+import Footer from '../../layout/Footer/Footer';
 
 const About = () => {
+  const { lightTheme } = useThemeObserver();
+
   return (
     <>
-      <div className={styles.About}>
-        <h1></h1>
-      </div>
+      <motion.div
+        exit={{ opacity: 0, y: -150 }}
+        initial={{ opacity: 0, y: 150 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <div className={styles.about}>
+         <img src='/images/s1.jpg' alt='' />
+         <img src='/images/s2.jpg' alt='' />
+         <img src='/images/s3.jpg' alt='' />
+        </div>
+        <Footer />
+      </motion.div>
     </>
   );
 };
