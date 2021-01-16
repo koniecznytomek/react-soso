be1345791b05b026019dd95c0ac3bf50cea80978
koniecznytomek <tomek@tomekkonieczny.com>
2021-01-16 10:46:09 +0100

    Turn on hints persistor

--- src/store/store.ts
+++ src/store/store.ts
@@ -17,7 +17,7 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  whitelist: ['cartState'],
+  whitelist: ['cartState', 'hintsState'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
