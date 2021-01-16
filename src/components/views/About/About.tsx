a8764d5df71b7dab610dc86f3cf38c135ca2d71b
koniecznytomek <tomek@tomekkonieczny.com>
2021-01-16 11:11:35 +0100

    Code cleanup

--- src/components/views/About/About.tsx
+++ src/components/views/About/About.tsx
@@ -3,8 +3,10 @@ import styles from './About.module.scss';
 import { useThemeObserver } from '../../../hooks/useThemeObserver';
 import { motion } from 'framer-motion';
 import Footer from '../../layout/Footer/Footer';
+import { useSlideImages } from '../../../hooks/useSlideImages';
 
 const About = () => {
+  const { slideImage } = useSlideImages();
   const { lightTheme } = useThemeObserver();
 
   useEffect(() => {
@@ -20,9 +22,57 @@ const About = () => {
         transition={{ duration: 0.8 }}
       >
         <div className={styles.about}>
-         <img src='/images/s1.jpg' alt='' />
-         <img src='/images/s2.jpg' alt='' />
-         <img src='/images/s3.jpg' alt='' />
+          <header className={styles.header}>
+            <div className={styles.header__images}>
+              <img src='/images/s1.jpg' alt='logo' />
+              <img src='/images/s2.jpg' alt='logo' />
+              <img src='/images/s3.jpg' alt='logo' />
+            </div>
+            <div className={styles.header__title} ref={slideImage}>
+              <h1>Sososocks brandingbook</h1>
+              <p>
+                <strong>
+                  A Brand Book was created to ensure overal brand consistancy
+                  when working with partners and marketers.
+                </strong>
+                At vero eos et accusamus et iusto odio dignissimos ducimus, qui
+                blanditiis praesentium voluptatum deleniti atque corrupti, quos
+                dolores et quas molestias excepturi sint, obcaecati cupiditate
+                non provident, similique sunt in culpa, qui officia deserunt
+                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+                rerum facilis est et expedita distinctio. Nam libero tempore,
+                cum soluta nobis est eligendi optio, cumque nihil impedit, quo
+                minus id, quod maxime placeat, facere possimus, omnis voluptas
+                assumenda est, omnis dolor repellendus. Temporibus autem
+                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+                eveniet, ut et voluptates repudiandae sint et molestiae non
+                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
+                ut aut reiciendis voluptatibus maiores alias consequatur aut
+                perferendis doloribus asperiores repellat.
+              </p>
+            </div>
+          </header>
+          <section className={styles.history} ref={lightTheme}>
+            <div className={styles.history__title}>
+              <h2>Our history</h2>
+              <p>
+                At vero eos et accusamus et iusto odio dignissimos ducimus, qui
+                blanditiis praesentium voluptatum deleniti atque corrupti, quos
+                dolores et quas molestias excepturi sint, obcaecati cupiditate
+                non provident, similique sunt in culpa, qui officia deserunt
+                mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+                rerum facilis est et expedita distinctio. Nam libero tempore,
+                cum soluta nobis est eligendi optio, cumque nihil impedit, quo
+                minus id, quod maxime placeat, facere possimus, omnis voluptas
+                assumenda est, omnis dolor repellendus. Temporibus autem
+                quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+                eveniet, ut et voluptates repudiandae sint et molestiae non
+                recusandae. Itaque earum rerum hic tenetur a sapiente delectus,
+                ut aut reiciendis voluptatibus maiores alias consequatur aut
+                perferendis doloribus asperiores repellat.
+              </p>
+            </div>
+          </section>
         </div>
         <Footer />
       </motion.div>
